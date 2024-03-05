6fa1baf916889ae70c5e6f39d41ffc9c806b9e06
Howard Nguyen-Huu <howardnguyenhuu@gmail.com>
2024-03-05 02:19:31 -0500

    p10k

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -81,11 +81,18 @@
       size = 10000;
       path = "$HOME/.config/zsh/history";
     };
-    oh-my-zsh = {
-      enable = true;
-      # plugins = [ "powerlevel10k" ];
-      theme = "robbyrussell";
-    };
+    plugins = [
+      {
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = ./p10k-config;
+        file = ".p10k.zsh";
+      }
+    ];
   };
 
   programs.vscode = {
