346208659746e19d1c51f520784e797c10753e65
Howard Nguyen-Huu <howardnguyenhuu@gmail.com>
2024-10-02 18:41:27 -0400

    add power settings for bofa

--- home-manager/hosts/bofa.nix
+++ home-manager/hosts/bofa.nix
@@ -10,4 +10,10 @@
     ../home.nix
     ../home-desktop.nix
   ];
+
+  config = {
+    programs.plasma.powerdevil.AC.autoSuspend.action = "nothing";
+    programs.plasma.powerdevil.AC.turnOffDisplay.idleTimeout = 600;
+    programs.plasma.powerdevil.AC.turnOffDisplay.idleTimeoutWhenLocked = "whenLockedAndUnlocked";
+  };
 }
