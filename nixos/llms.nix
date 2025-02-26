7d717136d938b628e09674e483b444f98e09f0fe
Howard Nguyen-Huu <howardnguyenhuu@gmail.com>
2025-02-25 21:03:35 -0500

    refactor graphics

--- nixos/llms.nix
+++ nixos/llms.nix
@@ -1,22 +1,24 @@
 {
-  inputs,
-  outputs,
   lib,
   config,
   pkgs,
   ...
 }: {
   services = {
-    ollama = rec {
-      enable = true;
-      package = pkgs.ollama-rocm;
-      acceleration = "rocm";
-      rocmOverrideGfx = "11.0.0";
+    ollama = {
+      package =
+        if config.myGraphics.amd
+        then pkgs.ollama-rocm
+        else pkgs.ollama-cuda;
+      acceleration =
+        if config.myGraphics.amd
+        then "rocm"
+        else "cuda";
+      rocmOverrideGfx = lib.mkIf config.myGraphics.amd "11.0.0";
     };
     open-webui = {
-      enable = true;
       environment = {
-        OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
+        OLLAMA_API_BASE_URL = lib.mkIf config.services.ollama.enable "http://127.0.0.1:11434";
         WEBUI_AUTH = "False";
         ANONYMIZED_TELEMETRY = "False";
         DO_NOT_TRACK = "True";
