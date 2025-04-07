265aa290bf6cf3700d9309a3ac857f0851736bd6
Howard Nguyen-Huu <howardnguyenhuu@gmail.com>
2025-04-07 16:59:40 -0400

    nvf git merge resolution

--- home-manager/editors/nvf/git.nix
+++ home-manager/editors/nvf/git.nix
@@ -1,10 +1,22 @@
-{...}: {
+{
   programs.nvf.settings.vim = {
-    git.enable = true;
-    git.gitsigns.enable = true;
-    git.gitsigns.codeActions.enable = false;
+    git = {
+      enable = true;
+      gitsigns.enable = true;
+      gitsigns.codeActions.enable = false;
 
-    git.vim-fugitive.enable = true;
+      git-conflict.enable = true;
+      git-conflict.mappings = {
+        both = "<leader>cb";
+        nextConflict = "[x";
+        none = "<leader>c0";
+        ours = "<leader>co";
+        prevConflict = "]x";
+        theirs = "<leader>ct";
+      };
+
+      vim-fugitive.enable = true;
+    };
     keymaps = [
       {
         key = "<leader>gs";
@@ -14,15 +26,5 @@
         desc = "open fugitive";
       }
     ];
-
-    git.git-conflict.enable = true;
-    git.git-conflict.mappings = {
-      both = "<leader>cb";
-      nextConflict = "[x";
-      none = "<leader>c0";
-      ours = "<leader>co";
-      prevConflict = "]x";
-      theirs = "<leader>ct";
-    };
   };
 }
