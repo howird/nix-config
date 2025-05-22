5dd02cb0e67bfddf72d67c517171cae8fa5e5dbf
Howard Nguyen-Huu <howardnguyenhuu@gmail.com>
2025-05-22 13:56:48 -0400

    fish wallpaper

--- home-manager/stylix.nix
+++ home-manager/stylix.nix
@@ -7,7 +7,14 @@
   options = with lib; {
     myWallpaper = mkOption {
       type = types.path;
-      default = ./desktops/wallpapers/pokemon-concierge.jpg;
+      default = let
+        catppuccin-wallpapers = pkgs.fetchFromGitHub {
+          owner = "zhichaoh";
+          repo = "catppuccin-wallpapers";
+          rev = "1023077979591cdeca76aae94e0359da1707a60e";
+          sha256 = "sha256-h+cFlTXvUVJPRMpk32jYVDDhHu1daWSezFcvhJqDpmU=";
+        };
+      in "${catppuccin-wallpapers}/misc/lonely-fish.png";
       description = "Wallpaper filename.";
     };
   };
