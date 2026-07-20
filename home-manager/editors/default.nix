1fbce112d67b83f6264c5377b0e5575c86e9cb86
Howard Nguyen-Huu <howardnguyenhuu@gmail.com>
2026-07-20 18:03:56 -0400

    add kiro code and remove codex

--- home-manager/editors/default.nix
+++ home-manager/editors/default.nix
@@ -12,7 +12,10 @@
   home.packages = with pkgs; [
     inputs.claude-desktop.packages.${system}.claude-desktop
     claude-code
+
+    kiro-fhs
+    kiro-cli
+
     antigravity-cli
-    codex
   ];
 }
