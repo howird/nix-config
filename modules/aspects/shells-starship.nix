fa0a8761303fa4a6eb1488ca46e5241e902090ca
Howard Nguyen-Huu <howardnguyenhuu@gmail.com>
2026-08-31 20:26:03 -0400

    starship fix

--- modules/aspects/shells-starship.nix
+++ modules/aspects/shells-starship.nix
@@ -11,8 +11,8 @@
     };
 
     programs.starship.settings = let
-      icon-fmt = c: s: "[](fg:${c})[${s}](fg:black bg:${c})"; # [](fg:${c} bg:black)
-      txt-fmt = txt: "[${txt}](fg:white bg:black)[](fg:black)";
+      icon-fmt = c: s: "[](fg:${c})[${s}](fg:black bg:${c})"; # [](fg:${c} bg:black)
+      txt-fmt = txt: "[${txt}](fg:white bg:black)[](fg:black)";
       icon-txt-fmt = c: s: txt: "${icon-fmt c s}${txt-fmt txt}";
 
       lang-fmt = c: s: {
@@ -32,7 +32,7 @@
 
       character = {
         format = "[╰─$symbol](fg:bright-black) ";
-        success_symbol = "[](fg:bold white)";
+        success_symbol = "[](fg:bold white)";
         error_symbol = "[×](fg:bold red)";
         disabled = true;
       };
@@ -43,7 +43,7 @@
       };
 
       status = {
-        symbol = " ";
+        symbol = " ";
         format = icon-txt-fmt "#E23140" "$symbol" " $status";
         pipestatus = true;
         pipestatus_separator = "-";
@@ -54,40 +54,40 @@
 
       directory = {
         format = icon-txt-fmt "brown" "󰷏 " " $read_only$truncation_symbol$path";
-        home_symbol = " ";
-        truncation_symbol = " ../";
+        home_symbol = " ";
+        truncation_symbol = " ../";
         truncation_length = 5;
         read_only = "󱧵 ";
         read_only_style = "";
         substitutions = {
-          "Desktop" = " ";
+          "Desktop" = " ";
           "Documents" = "󰈙 ";
-          "Downloads" = " ";
+          "Downloads" = " ";
           "Music" = "󰝚 ";
-          "Pictures" = " ";
-          "Videos" = " ";
+          "Pictures" = " ";
+          "Videos" = " ";
           "dev" = "󰲋 ";
-          "robo" = " ";
-          "config" = " ";
+          "robo" = " ";
+          "config" = " ";
         };
       };
 
-      git_branch.symbol = " ";
+      git_branch.symbol = " ";
       git_branch.format = ''${icon-fmt "green" "$symbol"}[ $branch](fg:white bg:black)'';
-      git_commit.tag_symbol = "  ";
+      git_commit.tag_symbol = "  ";
       git_status = {
         format = txt-fmt " $all_status";
         conflicted = " =";
         up_to_date = "";
-        untracked = "  $count";
-        stashed = "  $count";
-        modified = "  $count";
-        staged = "  $count";
-        renamed = "  $count";
+        untracked = "  $count";
+        stashed = "  $count";
+        modified = "  $count";
+        staged = "  $count";
+        renamed = "  $count";
         deleted = " ✘ $count";
-        ahead = "  $count";
+        ahead = "  $count";
         diverged = " 󰹹⇡$ahead_count⇣$behind_count";
-        behind = "  $count";
+        behind = "  $count";
       };
 
       battery = {
@@ -106,7 +106,7 @@
 
       cmd_duration = {
         min_time = 500;
-        format = icon-txt-fmt "orange" " " " $duration";
+        format = icon-txt-fmt "orange" " " " $duration";
       };
 
       time = {
@@ -119,12 +119,12 @@
         format = icon-fmt "red" "$symbol";
         disabled = false;
         symbols = {
-          Linux = " ";
-          Macos = " ";
-          NixOS = " ";
-          Ubuntu = " ";
+          Linux = " ";
+          Macos = " ";
+          NixOS = " ";
+          Ubuntu = " ";
           Windows = "󰍲 ";
-          Unknown = " ";
+          Unknown = " ";
         };
       };
 
@@ -135,54 +135,54 @@
 
       hostname = {
         format = txt-fmt "@$hostname$ssh_symbol";
-        ssh_symbol = "  ";
+        ssh_symbol = "  ";
         ssh_only = false;
       };
 
-      aws = lang-fmt "orange" "  ";
-      buf = lang-fmt "blue" " ";
-      bun = lang-fmt "cyan" " ";
-      c = lang-fmt "blue" " ";
-      cmake = lang-fmt "red" " ";
-      conda = lang-fmt "green" " ";
-      cpp = lang-fmt "blue" " ";
-      crystal = lang-fmt "cyan" " ";
-      dart = lang-fmt "blue" " ";
-      deno = lang-fmt "green" " ";
-      docker_context = lang-fmt "blue" " ";
-      elixir = lang-fmt "magenta" " ";
-      elm = lang-fmt "blue" " ";
-      fennel = lang-fmt "brown" " ";
-      fossil_branch = lang-fmt "green" " ";
-      gcloud = lang-fmt "blue" "  ";
-      golang = lang-fmt "blue" " ";
-      gradle = lang-fmt "green" " ";
-      guix_shell = lang-fmt "blue" " ";
-      haskell = lang-fmt "blue" " ";
-      haxe = lang-fmt "orange" " ";
-      hg_branch = lang-fmt "green" " ";
-      java = lang-fmt "brown" " ";
-      julia = lang-fmt "purple" " ";
-      kotlin = lang-fmt "orange" " ";
-      lua = lang-fmt "blue" " ";
+      aws = lang-fmt "orange" "  ";
+      buf = lang-fmt "blue" " ";
+      bun = lang-fmt "cyan" " ";
+      c = lang-fmt "blue" " ";
+      cmake = lang-fmt "red" " ";
+      conda = lang-fmt "green" " ";
+      cpp = lang-fmt "blue" " ";
+      crystal = lang-fmt "cyan" " ";
+      dart = lang-fmt "blue" " ";
+      deno = lang-fmt "green" " ";
+      docker_context = lang-fmt "blue" " ";
+      elixir = lang-fmt "magenta" " ";
+      elm = lang-fmt "blue" " ";
+      fennel = lang-fmt "brown" " ";
+      fossil_branch = lang-fmt "green" " ";
+      gcloud = lang-fmt "blue" "  ";
+      golang = lang-fmt "blue" " ";
+      gradle = lang-fmt "green" " ";
+      guix_shell = lang-fmt "blue" " ";
+      haskell = lang-fmt "blue" " ";
+      haxe = lang-fmt "orange" " ";
+      hg_branch = lang-fmt "green" " ";
+      java = lang-fmt "brown" " ";
+      julia = lang-fmt "purple" " ";
+      kotlin = lang-fmt "orange" " ";
+      lua = lang-fmt "blue" " ";
       memory_usage = lang-fmt "blue" "󰍛 ";
       meson = lang-fmt "green" "󰔷 ";
       nim = lang-fmt "yellow" "󰆥 ";
-      nix_shell = lang-env-fmt "blue" " " " $state (\\($name\\))";
-      nodejs = lang-fmt "green" " ";
-      ocaml = lang-fmt "orange" " ";
+      nix_shell = lang-env-fmt "blue" " " " $state (\\($name\\))";
+      nodejs = lang-fmt "green" " ";
+      ocaml = lang-fmt "orange" " ";
       package = lang-fmt "green" "󰏗 ";
-      perl = lang-fmt "blue" " ";
-      php = lang-fmt "blue" " ";
-      pijul_channel = lang-fmt "green" " ";
+      perl = lang-fmt "blue" " ";
+      php = lang-fmt "blue" " ";
+      pijul_channel = lang-fmt "green" " ";
       pixi = lang-fmt "yellow" "󰏗 ";
-      python = lang-env-fmt "yellow" " " " $version (\\($virtualenv\\))";
+      python = lang-env-fmt "yellow" " " " $version (\\($virtualenv\\))";
       rlang = lang-fmt "blue" "󰟔 ";
-      ruby = lang-fmt "red" " ";
+      ruby = lang-fmt "red" " ";
       rust = lang-fmt "orange" "󱘗 ";
-      scala = lang-fmt "red" " ";
-      swift = lang-fmt "orange" " ";
-      zig = lang-fmt "orange" " ";
+      scala = lang-fmt "red" " ";
+      swift = lang-fmt "orange" " ";
+      zig = lang-fmt "orange" " ";
 
       palette = lib.mkDefault "catppuccin_mocha";
       palettes.catppuccin_mocha = {
